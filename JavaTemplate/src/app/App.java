4a169eb8c8f48a95c2db2c1173f4853bd2375590
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-11-05 14:48:15 +0000

    Created Assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,7 +1,20 @@
+/*
+ARRAY EASY PRACTICE
+
+Create a program that lists your top 10 favorite movies. 
+
+This is similar to an assignment you had at the beginnning of the trimester, but now you have limitations.
+
+LIMITATIONS:
+You may only use one array variable to store your movies.
+
+*/
+
+
 package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+       
     }
 }
